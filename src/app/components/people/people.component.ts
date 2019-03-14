357740d5e12c8e00ea021c65fab71ed69a64b823
Nikola Rusakov <nikola.rusakov@flowup.cz>
2019-03-14 16:43:34 +0100

    Revert "feat: swapi api call"
    This reverts commit a04d1817

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Apollo} from 'apollo-angular';
 import {map, withLatestFrom} from 'rxjs/operators';
 import keysToCamel from 'src/app/utils/converting-keys';
+import {swapiPeopleFilm} from '../../gql/fragments';
 import {GQL_NODE_QUERY, GQL_PERSON_FILM_QUERY, SWAPI_PEOPLE_FILM_QUERY, SWAPI_PEOPLE_ONLY_QUERY} from '../../gql/queries';
 import {MatDialog} from '@angular/material';
 import {DetailDialogComponent} from '../detail-dialog/detail-dialog.component';
@@ -14,13 +15,13 @@ import {DetailDialogComponent} from '../detail-dialog/detail-dialog.component';
 export class PeopleComponent implements OnInit {
 
   details =
-    // this.apollo.use('extra').watchQuery({
-    //     query: GQL_PERSON_FILM_QUERY
-    this.apollo.watchQuery({
-    query: SWAPI_PEOPLE_FILM_QUERY
+    this.apollo.use('extra').watchQuery({
+        query: GQL_PERSON_FILM_QUERY
+    // this.apollo.watchQuery({
+    // query: SWAPI_PEOPLE_FILM_QUERY
   }).valueChanges.pipe(
     // @ts-ignore
-    map(value => keysToCamel(value).data.people));
+    map(value => keysToCamel(value).data.allPeople.people));
 
   constructor(
     private readonly apollo: Apollo,
